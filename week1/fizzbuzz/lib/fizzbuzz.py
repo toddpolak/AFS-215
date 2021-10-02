5d5fd24383216ef6571e17785798f078253b8880
toddpolak <67641770+toddpolak@users.noreply.github.com>
2021-10-01 22:26:22 -0500

    Assignment 1.2: Python Kata

---
+++ week1/fizzbuzz/lib/fizzbuzz.py
@@ -0,0 +1,6 @@
+def fizzBuzz(num):
+    if (num == 1):
+        return '1'
+    elif (num == 2):
+        return '2'
+    return "something else"
---
+++ week1/fizzbuzz/test/test_fizzbuzz.py
@@ -0,0 +1,15 @@
+import pytest
+from lib.fizzbuzz import fizzBuzz
+
+def test1_fizzBuzz():
+    test = fizzBuzz(1)
+    assert test == '1'
+    
+def test2_fizzBuzz():
+    test = fizzBuzz(2)
+    assert test == '2'
+    
+def test3_fizzBuzz():
+    test = fizzBuzz(3)
+    assert test == '3'
+    
