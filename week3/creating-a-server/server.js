efa373eb3579c0bf6fc08e0fddc5c8f9383a5e39
toddpolak <67641770+toddpolak@users.noreply.github.com>
2021-10-14 18:08:13 -0500

    Update server.js

--- week3/creating-a-server/server.js
+++ week3/creating-a-server/server.js
@@ -5,6 +5,8 @@ const reqListener = (req, res) => {
     res.end('Node JS Server')
 }
 
+// Test Server
+
 const server = http.createServer(reqListener)
 server.listen(3000, () => {
     console.log('The server is listening on port 3000 ..')
