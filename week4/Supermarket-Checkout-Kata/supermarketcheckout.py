952c6f3698644e62585b252bd43697f070d870af
toddpolak <67641770+toddpolak@users.noreply.github.com>
2021-10-26 10:55:25 -0500

    TDD Supermarket Checkout

--- week4/Supermarket-Checkout-Kata/supermarketcheckout.py
+++ week4/Supermarket-Checkout-Kata/supermarketcheckout.py
@@ -5,31 +5,54 @@ class Product():
             raise ValueError('Price is missing')
         else:
             self.price = price
+            
+class DiscountRule():
+    def __init__(self, discount):
+        self.discount = discount
 
 class Checkout():
     def __init__(self):
         self.items = []
+        self.discount_rules = []
         
     def add_product(self, product):
         self.items.append(product)
         
+    def add_discount_rule(self, discount):
+        self.discount_rules.append(discount)
+
     def calculate_current_total(self):
         cart_items = self.items
         cart_total = 0
         
         for item in cart_items:
             cart_total += item.price
+
+        for rule in self.discount_rules:
+            cart_discount = 0
             
+            print('rule: ', rule.discount)
+            
+            cart_discount = cart_total * rule.discount
+            
+            cart_total -= round(cart_discount, 2)
+            
+            print('discount: ', cart_discount)
+
         return cart_total
         
         
 
         
 
-#myCheckout = Checkout()
+myCheckout = Checkout()
 
-#product1 = Product('Milk', 3)
+product1 = Product('Milk', 3)
 
-#myCheckout.add_product(product1)
+myCheckout.add_product(product1)
 
-#print('Cart Total: ', '$' + str(myCheckout.calculate_current_total()))
+amount1 = DiscountRule(.05)
+
+myCheckout.add_discount_rule(amount1)
+
+print('Cart Total: ', '$' + str(myCheckout.calculate_current_total()))
