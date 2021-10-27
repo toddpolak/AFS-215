32225ae3855d4cdd9f07a19df28b3cb59f744050
toddpolak <67641770+toddpolak@users.noreply.github.com>
2021-10-26 19:55:45 -0500

    TDD Supermarket Checkout

--- week4/Supermarket-Checkout-Kata/supermarketcheckout.py
+++ week4/Supermarket-Checkout-Kata/supermarketcheckout.py
@@ -29,54 +29,16 @@ class Checkout():
         for item in cart_items:
             cart_total += item.price
 
-        #for rule in self.discount_rules:
-            #cart_discount = 0
-            
-            #print('rule: ', rule.discount)
-            
-            #cart_discount = cart_total * rule.discount
-            
-            #cart_total -= round(cart_discount, 2)
-            
-            #print('discount: ', cart_discount)
-
         return cart_total
     
     def apply_discount(self):
         cart_items = self.items
         discount_total = 0
-        #cart_total = 0
-        
+
         for rule in self.discount_rules:
-            print('product: ', rule.product)
-            print('discount: ', rule.discount)
-        
             for item in cart_items:
-                print('name: ', item.name)
-                print('price: ', item.price)
-        
                 if item.name == rule.product:
                     discount_total += rule.discount
                     break
-                    
-        print('Discount Total: ', discount_total)
-        print('Prev Total: ', self.calculate_current_total())
-        
+    
         return int(self.calculate_current_total()) - discount_total
-        
-        
-        
-
-myCheckout = Checkout()
-
-product1 = Product('Milk', 3)
-
-myCheckout.add_product(product1)
-
-rule1 = DiscountRule('Milk', .25)
-
-myCheckout.add_discount_rule(rule1)
-
-print('Cart Total: ', '$' + str(myCheckout.calculate_current_total()))
-
-print('New Total: ', myCheckout.apply_discount())
--- week4/Supermarket-Checkout-Kata/test_supermarketcheckout.py
+++ week4/Supermarket-Checkout-Kata/test_supermarketcheckout.py
@@ -1,5 +1,4 @@
-from supermarketcheckout import Product
-from supermarketcheckout import Checkout
+from supermarketcheckout import Product, DiscountRule, Checkout
 
 myCheckout = Checkout()
 
@@ -7,8 +6,11 @@ def test_Instantiate():
     assert type (myCheckout) is Checkout
     
 def test_AddItem():
-    #item = Product('Milk', 3)
     myCheckout.add_product(Product('Milk', 3))
+    
+def test_CalculateCurrentTotal():
+    total = myCheckout.calculate_current_total()
+    assert total == 3
     
 def test_AddMultipleItems():
     myCheckout.add_product(Product('Eggs', 2))
@@ -19,6 +21,11 @@ def test_CalculateCurrentTotal():
     total = myCheckout.calculate_current_total()
     assert total == 13
     
+def test_AddDiscountRule():
+    myCheckout.add_discount_rule(DiscountRule('Milk', .25))
+    
+def test_ApplyDiscount():
+    myCheckout.apply_discount()
+    
 def test_exception_AddWithoutPrice():
-    #item = Product('Bread')
     myCheckout.add_product(Product('Bread'))
