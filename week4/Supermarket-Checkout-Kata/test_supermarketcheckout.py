c56e5dc1a50b314b4c2082e3a784284cc9af078d
toddpolak <67641770+toddpolak@users.noreply.github.com>
2021-10-26 22:16:32 -0500

    TDD Supermarket Checkout

--- week4/Supermarket-Checkout-Kata/test_supermarketcheckout.py
+++ week4/Supermarket-Checkout-Kata/test_supermarketcheckout.py
@@ -1,4 +1,5 @@
 from supermarketcheckout import Product, DiscountRule, Checkout
+import pytest
 
 myCheckout = Checkout()
 
@@ -17,7 +18,7 @@ def test_AddMultipleItems():
     myCheckout.add_product(Product('Meat', 5))
     myCheckout.add_product(Product('Cereal', 3))
     
-def test_CalculateCurrentTotal():
+def test_RecalculateCurrentTotal():
     total = myCheckout.calculate_current_total()
     assert total == 13
     
@@ -25,7 +26,10 @@ def test_AddDiscountRule():
     myCheckout.add_discount_rule(DiscountRule('Milk', .25))
     
 def test_ApplyDiscount():
-    myCheckout.apply_discount()
+    total = myCheckout.apply_discount()
+    assert total == 12.75
     
 def test_exception_AddWithoutPrice():
-    myCheckout.add_product(Product('Bread'))
+    with pytest.raises(Exception) as e_info:
+        myCheckout.add_product(Product('Bread'))
+    
