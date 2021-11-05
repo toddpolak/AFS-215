5c7f20c441c0ddb69666ccfdb3317f36adbd2175
toddpolak <67641770+toddpolak@users.noreply.github.com>
2021-11-04 21:47:16 -0500

    Node.js Kata

--- week5/fizzbuzzkata/test/fizzbuzzkata.test.js
+++ week5/fizzbuzzkata/test/fizzbuzzkata.test.js
@@ -1,11 +1,19 @@
-import { expect } from 'chai'
+import { assert, expect } from 'chai'
 import fizzbuzz from '../Fizzbuzzkata.js'
 
 describe('Test', () => {
     it('returns 1 when pass in a 1', () => {
-        expect(fizzbuzz(1)).to.equal('1')
+        assert.equal(fizzbuzz(1), '1')
     })
     it('returns 2 when pass in a 2', () => {
-        expect(fizzbuzz(2)).to.equal('2')
+        assert.equal(fizzbuzz(2), '2')
+    })
+    it('returns Incorrect when pass in anything except a 1 or a 2', () => {
+        expect(fizzbuzz(3)).to.equal('Incorrect')
+    })
+
+    // Should fail:
+    it('returns Incorrect when pass in anything except a 1 or a 2', () => {
+        expect(fizzbuzz(3)).to.equal('3')
     })
 })
